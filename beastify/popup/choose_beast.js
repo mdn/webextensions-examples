8f2d4109ee1e16f0e7ce023842452ae2bd1a2839
Will Bamberg <wbamberg@mozilla.com>
2015-10-23 14:19:04 -0700

    work around bug 1217944; remove unecessary use of callback argument

--- beastify/popup/choose_beast.js
+++ beastify/popup/choose_beast.js
@@ -21,12 +21,10 @@ document.addEventListener("click", function(e) {
   
   chrome.tabs.executeScript(null, {
     file: "content_scripts/beastify.js" 
-  }, setBeast);
+  });
 
-  function setBeast() {  
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {beast: chosenBeast});
-      });
-    }
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {beast: chosenBeast});
+  });
 
 });
