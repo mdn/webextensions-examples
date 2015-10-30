5895c5c6bd9d26ba5eb0742e473ab09ff16de26b
Will Bamberg <wbamberg@mozilla.com>
2015-10-30 10:13:18 -0700

    added logging statements

--- notify-link-clicks/background-script.js
+++ notify-link-clicks/background-script.js
@@ -1,6 +1,7 @@
 chrome.runtime.onMessage.addListener(notify);
 
 function notify(message) {
+  console.log("background script received message");
   chrome.notifications.create({
     "type": "basic",
     "iconUrl": chrome.extension.getURL("link.png"),
--- notify-link-clicks/content-script.js
+++ notify-link-clicks/content-script.js
@@ -1,6 +1,7 @@
 window.addEventListener("click", notifyExtension);
 
 function notifyExtension(e) {
+  console.log("content script sending message");
   if (e.target.tagName != "A") {
     return;
   }
