4de58493ff65c214159c984ad0b410d369f17ecc
Will Bamberg <wbamberg@mozilla.com>
2015-11-03 15:41:26 -0800

    added localization for manifest.json strings

--- notify-link-clicks-i18n/background-script.js
+++ notify-link-clicks-i18n/background-script.js
@@ -2,12 +2,12 @@ chrome.runtime.onMessage.addListener(notify);
 
 function notify(message) {
   console.log("background script received message");
-  var notificationTitle = chrome.i18n.getMessage("notify-title");
-  var notificationContent = chrome.i18n.getMessage("notify-content", message.url);
+  var title = chrome.i18n.getMessage("notificationTitle");
+  var content = chrome.i18n.getMessage("notificationContent", message.url);
   chrome.notifications.create({
     "type": "basic",
     "iconUrl": chrome.extension.getURL("link.png"),
-    "title": notificationTitle,
-    "message": notificationContent
+    "title": title,
+    "message": content
   });
 }
