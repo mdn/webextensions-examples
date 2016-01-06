13c506fb490e9c995de57630cc2685668468e4d2
Will Bamberg <wbamberg@mozilla.com>
2016-01-06 13:35:07 -0800

    corrected a  comment

--- beastify/popup/choose_beast.js
+++ beastify/popup/choose_beast.js
@@ -5,12 +5,10 @@ If the click is not on one of the beasts, return early.
 
 Otherwise, the text content of the node is the name of the beast we want.
 
-Run the "beastify.js" content script in the active tab, calling setBeast()
-once the content script has executed.
+Inject the "beastify.js" content script in the active tab.
 
-Inside setBeast(), get the active tab, then send it a message containing
-the chosen beast's name. This message will be received by the content script
-we just executed.
+Then get the active tab and send "beastify.js" a message
+containing the chosen beast's name.
 */
 document.addEventListener("click", function(e) {
   if (!e.target.classList.contains("beast")) {
