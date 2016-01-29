364b1f029e431e5bbd3e6daf429757c1a7a3e952
Luca Greco <luca.greco@alcacoop.it>
2016-01-29 12:37:38 +0100

    Apply minor tweaks and fix typos in inpage-toolbar-ui doc and comments

--- inpage-toolbar-ui/toolbar/ui.js
+++ inpage-toolbar-ui/toolbar/ui.js
@@ -3,6 +3,6 @@ var port = chrome.runtime.connect();
 
 // Handle click events on the toolbar button.
 document.querySelector("#toggle").addEventListener("click", function() {
-  // Ask to the background page to toggle the toolbar on the current tab
+  // Ask the background page to toggle the toolbar on the current tab
   port.postMessage("toggle-in-page-toolbar");
 });
