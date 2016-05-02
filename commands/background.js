dc4b7fe4eae7f76857c7e72ae71f04c04a51c6cb
Mindaugas Jakutis <4mr.minj@gmail.com>
2016-05-02 13:30:03 +0300

    switch to using Ctrl+Shift+U in commands example
    seems to be assigned to download manager (at least on linux)

--- commands/background.js
+++ commands/background.js
@@ -7,7 +7,7 @@
  *    [{
  *       name: "toggle-feature",
  *       description: "Send a 'toggle-feature' event to the extension"
- *       shortcut: "Ctrl+Shift+Y"
+ *       shortcut: "Ctrl+Shift+U"
  *    }]
  */
 chrome.commands.getAll(function(commandsArray) {
@@ -19,8 +19,8 @@ chrome.commands.getAll(function(commandsArray) {
 /**
  * Fired when a registered command is activated using a keyboard shortcut.
  *
- * In this sample extension, there is only one registered command: "Ctrl+Shift+Y".
- * On Mac, this command will automatically be converted to "Command+Shift+Y".
+ * In this sample extension, there is only one registered command: "Ctrl+Shift+U".
+ * On Mac, this command will automatically be converted to "Command+Shift+U".
  */
 chrome.commands.onCommand.addListener(function(command) {
   console.log("onCommand event received for message: ", command);
