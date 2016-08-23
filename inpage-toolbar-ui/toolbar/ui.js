71ecdfacd29a16f2e1a29a89a6103f36c30d1d6a
Luca Greco <luca.greco@alcacoop.it>
2016-08-23 15:33:43 +0200

    remove inpage-toolbar-ui example
    Based on the discussion in #74, we decided to remove this example
    to prevent it to seem as the suggested way to expose
    "security/privacy" sensible UI elements to a potentially malicious page.
    
    Even if there are probably reasonable scenarios where this feature
    can be used, it is currently perceived as a replacement for a
    browser toolbar UI element, which is not.

--- inpage-toolbar-ui/toolbar/ui.js
+++
@@ -1,8 +0,0 @@
-// Connect to the background page.
-var port = chrome.runtime.connect();
-
-// Handle click events on the toolbar button.
-document.querySelector("#toggle").addEventListener("click", function() {
-  // Ask the background page to toggle the toolbar on the current tab
-  port.postMessage("toggle-in-page-toolbar");
-});
