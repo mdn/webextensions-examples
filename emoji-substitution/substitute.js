af44ba1fc9424e44b327fe7667c4d3853f9322e7
Andrew Terranova <andrew.terranova@bazaarvoice.com>
2016-10-20 17:35:30 -0500

    Monitors DOM for additions after initial load

--- emoji-substitution/substitute.js
+++ emoji-substitution/substitute.js
@@ -65,3 +65,22 @@ function replaceText (element) {
 
 // Start the recursion from the body tag.
 replaceText(document.body);
+
+// Now monitor the DOM for additions and substitute emoji into new nodes.
+// @see https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver.
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.addedNodes && mutation.addedNodes.length > 0) {
+      // This DOM change was new nodes being added. Run our substitution
+      // algorithm on each newly added node.
+      for (let i = 0; i < mutation.addedNodes.length; i++) {
+        const newNode = mutation.addedNodes[i];
+        replaceText(newNode);
+      }
+    }
+  });
+});
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
