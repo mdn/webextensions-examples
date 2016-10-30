fd1308a9c85ca5dc741834cb4f02a2ea9f9c7f07
iampeterbanjo <peterbanjo@gmail.com>
2016-10-30 10:53:24 +0100

    show seed data and refactor

--- notes-with-indexeddb/notes.js
+++ notes-with-indexeddb/notes.js
@@ -1,12 +1,12 @@
 /*
 Feature detection
  */
-// DON'T use "var indexedDB = ..." if you're not in a function.
+// DON'T use 'var indexedDB = ...' if you're not in a function.
 window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
 
 // This line should only be needed if it is needed to support the object's constants for older browsers
 window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {
-    READ_WRITE: "readwrite"
+    READ_WRITE: 'readwrite'
 };
 
 window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
@@ -27,47 +27,98 @@ function showErrorMessage(event) {
     alert('IndexedDB request denied');
 }
 
-/*
-notesApp
- */
-function notesApp(event) {
-    var db = event.target.result;
+function addData(db, data, success) {
+    var store = db.transaction(['notes'], 'readwrite').objectStore('notes'),
+        request = store.add(data);
+
+    request.onerror = showErrorMessage;
+    request.onsuccess = success;
+}
+
+function createIndex(event) {
+    var db = event.target.result,
+        store = db.createObjectStore('notes', {
+            autoIncrement: true
+        });
 
     db.onerror = showErrorMessage;
-}
-
-const seedData = [{
-    title: "To-do",
-    description: "Buy cheese"
-}, {
-    title: "Books I like",
-    description: "Peak: the science of expertise"
-}];
-
-function populateDb(event) {
-    var db = event.target.result;
-
-    var store = db.createObjectStore("notes", {
-        autoIncrement: true
-    });
 
     // create indices to search db by
-    store.createIndex("title", "title", {
+    store.createIndex('title', 'title', {
         unique: false
     });
-    store.createIndex("description", "description", {
+    store.createIndex('description', 'description', {
         unique: false
     });
-
-    for (var row of seedData) {
-        store.add(seedData[row]);
-    }
 }
 
-var request = window.indexedDB.open("MyNotes", 1);
+/*
+populateDb with data
+ */
+function populateDb(db, data) {
+    data.forEach(function(row) {
+        addData(db, row);
+    });
+}
 
-request.onerror = showErrorMessage;
-// if this fails a request error is thrown
-request.onupgradeneeded = populateDb;
+function showNotes(db) {
+    var list = document.createDocumentFragment(),
+        notes = document.getElementById('notes');
 
-request.onsuccess = notesApp;
+    /*
+    listNotes with cursor
+     */
+    function listNotes(event) {
+        var cursor = event.target.result;
+
+        if (cursor) {
+            var title = document.createElement('h3'),
+                description = document.createElement('p'),
+                item = document.createElement('div');
+
+            title.innerHTML = cursor.value.title;
+            description.innerHTML = cursor.value.description;
+
+            item.appendChild(title);
+            item.appendChild(description);
+            list.appendChild(item);
+
+            cursor.continue();
+        } else {
+            notes.innerHTML = '';
+            notes.appendChild(list);
+        }
+    }
+
+    var store = db.transaction('notes').objectStore('notes');
+
+    store.openCursor().onsuccess = listNotes;
+}
+
+function notesApp(event) {
+    const seedData = [{
+        title: 'To-do',
+        description: 'Buy cheese'
+    }, {
+        title: 'Books I like',
+        description: 'Peak: the science of expertise'
+    }];
+
+    var db = event.target.result;
+
+    populateDb(db, seedData);
+    showNotes(db);
+}
+
+/*
+Open and run IndexedDb
+ */
+function start() {
+    var request = window.indexedDB.open('MyNotes', 1);
+
+    request.onerror = showErrorMessage;
+    request.onupgradeneeded = createIndex;
+    request.onsuccess = notesApp;
+}
+
+document.addEventListener('DOMContentLoaded', start);
