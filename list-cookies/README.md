b7048d83df165403ff93150b40f54ef1b90f244d
Flávio da Silva Rodrigues Almeida <darkflames@gmail.com>
2016-10-31 15:34:16 -0200

    Created WebExtension showing how to use the Cookies API (#114)

---
+++ list-cookies/README.md
@@ -0,0 +1,9 @@
+# list-cookies
+
+## What it does
+
+This extensions list the cookies in the active tab.
+
+# What it shows
+
+Demonstration of the getAll() function in the cookie API
---
+++ list-cookies/cookies.js
@@ -0,0 +1,35 @@
+function showCookiesForTab(tabs) {
+  //get the first tab object in the array
+  tab = tabs.pop();
+
+  //get all cookies in the domain
+  chrome.cookies.getAll({url: tab.url},  (cookies) => {
+
+    //set the header of the panel
+    var activeTabUrl = document.getElementById('header-title');
+    var text = document.createTextNode("Cookies at: "+tab.title);
+    var cookieList = document.getElementById('cookie-list');
+    activeTabUrl.appendChild(text);
+
+    if (cookies.length > 0) {
+      //add an <li> item with the name and value of the cookie to the list
+      for (cookie of cookies) {
+        var li = document.createElement("li");
+        var content = document.createTextNode(cookie.name + ": "+ cookie.value);
+        li.appendChild(content);
+        cookieList.appendChild(li);
+      }
+    } else {
+      var p = document.createElement("p");
+      var content = document.createTextNode("No cookies in this tab.");
+      var parent = cookieList.parentNode;
+
+      p.appendChild(content);
+      parent.appendChild(p);
+    }
+  });
+};
+
+//get active tab to run an callback function.
+//it sends to our callback an array of tab objects
+chrome.tabs.query({currentWindow: true, active: true}, showCookiesForTab);
