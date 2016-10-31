ee7e1af68f5b5903e10db1ff17f661b6ea986129
Flávio Rodrigues <flavio.rodrigues@talk2.com.br>
2016-10-31 11:30:27 -0200

    removed the callOnActiveTab function. Started using the tabs.query directly

--- list-cookies/cookies.js
+++ list-cookies/cookies.js
@@ -1,7 +1,9 @@
-//Execute code on active
-callOnActiveTab((tab) => {
+function showCookiesForTab(tabs) {
+  //get the first tab object in the array
+  tab = tabs.pop();
+
   //get all cookies in the domain
-  chrome.cookies.getAll({url: tab.url},  function(cookies) {
+  chrome.cookies.getAll({url: tab.url},  (cookies) => {
 
     //set the header of the panel
     var activeTabUrl = document.getElementById('header-title');
@@ -18,23 +20,16 @@ callOnActiveTab((tab) => {
         cookieList.appendChild(li);
       }
     } else {
-      var p = document.createElement("p")
+      var p = document.createElement("p");
       var content = document.createTextNode("No cookies in this tab.");
-      var parent = cookieList.parentNode
+      var parent = cookieList.parentNode;
 
       p.appendChild(content);
       parent.appendChild(p);
     }
   });
-});
+};
 
-//get active tab to run an callback function. Taken from tabs-tabs-tabs example
-function callOnActiveTab(callback) {
-  chrome.tabs.query({currentWindow: true}, function(tabs) {
-    for (var tab of tabs) {
-      if (tab.active) {
-        callback(tab, tabs);
-      }
-    }
-  });
-}
+//get active tab to run an callback function.
+//it sends to our callback an array of tab objects
+chrome.tabs.query({currentWindow: true, active: true}, showCookiesForTab);
