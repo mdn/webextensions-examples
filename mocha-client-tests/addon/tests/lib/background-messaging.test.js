3b59bd51a637202164c0714e9065a2d1bf6a13b8
Khmel Mykyta <hmelenok@gmail.com>
2016-11-05 19:49:19 +0200

    removed should to expect

--- mocha-client-tests/addon/tests/lib/background-messaging.test.js
+++ mocha-client-tests/addon/tests/lib/background-messaging.test.js
@@ -2,7 +2,7 @@ describe('Background', function() {
     describe('ping', function() {
         it('should return pong in response', function(done) {
             chrome.runtime.sendMessage({action: 'ping'},function(response) {
-                response.should.equal('pong');
+                expect(response).to.equal('pong');
                 done();
             });
         });
