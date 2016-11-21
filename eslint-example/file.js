dcdd025cb2dacafc5dda8d8dd403ead42b32dcb1
Kumar McMillan <kumar.mcmillan@gmail.com>
2016-11-21 10:20:26 -0600

    fixed readme, added clarifying comments to the source (#147)

--- eslint-example/file.js
+++ eslint-example/file.js
@@ -1,3 +1,6 @@
+// This special eslint comment will declare that the named
+// function has been "exported" into the global scope.
+
 /* exported getUsefulContents */
 function getUsefulContents(callback) {
     callback('Hello World');
