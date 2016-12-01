dc6233a53754968fbe4c0c7f2f8bdadcc5d04bb7
sneakypete81 <sneakypete81@gmail.com>
2016-12-01 18:17:55 +0000

    Handle asynchronous messaging

--- mocha-client-tests/tests/lib/background.test.js
+++ mocha-client-tests/tests/lib/background.test.js
@@ -1,8 +1,9 @@
 describe('Background', function() {
     describe('ping', function() {
-        it('should return pong in response', function() {
+        it('should return pong in response', function(done) {
             Background.ping(false, false, function(response) {
                 expect(response).to.equal('pong');
+                done();
             });
         });
     });
