fb18fbd37709236789110d87421060e15c307a54
Kumar McMillan <kumar.mcmillan@gmail.com>
2016-12-09 10:18:05 -1000

    Added comments to the webpack config

--- react-es6-popup/webpack.config.js
+++ react-es6-popup/webpack.config.js
@@ -3,21 +3,29 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: {
+    // Each entry in here would declare a file that needs to be transpiled
+    // and included in the extension source.
+    // For example, you could add a background script like:
+    // background: './src/background',
     popup: './src/popup',
   },
   output: {
+    // This copies each source entry into the extension dist folder named
+    // after its entry config key.
     path: 'extension/dist',
     filename: '[name].js',
   },
   module: {
+    // This transpiles all code (except for third party modules) using Babel.
     loaders: [{
       exclude: /node_modules/,
       test: /\.js$/,
-      // babel options are in .babelrc
+      // Babel options are in .babelrc
       loaders: ['babel'],
     }],
   },
   resolve: {
+    // This allows you to import modules just like you would in a NodeJS app.
     extensions: ['', '.js', '.jsx'],
     root: [
       path.resolve(__dirname),
@@ -28,9 +36,13 @@ module.exports = {
     ],
   },
   plugins: [
+    // Since some NodeJS modules expect to be running in Node, it is helpful
+    // to set this environment var to avoid reference errors.
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
   ],
+  // This will expose source map files so that errors will point to your
+  // original source files instead of the transpiled files.
   devtool: 'sourcemap',
 };
