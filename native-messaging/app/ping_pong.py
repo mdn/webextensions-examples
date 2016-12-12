b7c353d4d435b9bd9b11165153c2648ce84521b5
Andy McKay <amckay@mozilla.com>
2016-12-12 10:03:37 -0800

    make python pep 8 compliant

--- native-messaging/app/ping_pong.py
+++ native-messaging/app/ping_pong.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python
+import json
+import struct
+import sys
 
-import sys, json, struct
 
 # Read a message from stdin and decode it.
 def getMessage():
-  rawLength = sys.stdin.read(4)
-  if len(rawLength) == 0:
-      sys.exit(0)
-  messageLength = struct.unpack('@I', rawLength)[0]
-  message = sys.stdin.read(messageLength)
-  return json.loads(message)
+    rawLength = sys.stdin.read(4)
+    if len(rawLength) == 0:
+        sys.exit(0)
+    messageLength = struct.unpack('@I', rawLength)[0]
+    message = sys.stdin.read(messageLength)
+    return json.loads(message)
+
 
 # Encode a message for transmission,
 # given its content.
 def encodeMessage(messageContent):
-  encodedContent = json.dumps(messageContent)
-  encodedLength = struct.pack('@I', len(encodedContent))
-  return {'length': encodedLength, 'content': encodedContent}
+    encodedContent = json.dumps(messageContent)
+    encodedLength = struct.pack('@I', len(encodedContent))
+    return {'length': encodedLength, 'content': encodedContent}
+
 
 # Send an encoded message to stdout
 def sendMessage(encodedMessage):
-  sys.stdout.write(encodedMessage['length'])
-  sys.stdout.write(encodedMessage['content'])
-  sys.stdout.flush()
+    sys.stdout.write(encodedMessage['length'])
+    sys.stdout.write(encodedMessage['content'])
+    sys.stdout.flush()
 
 while True:
-  receivedMessage = getMessage()
-  if (receivedMessage == "ping"):
-    sendMessage(encodeMessage("pong"))
+    receivedMessage = getMessage()
+    if receivedMessage == 'ping':
+        sendMessage(encodeMessage('pong'))
