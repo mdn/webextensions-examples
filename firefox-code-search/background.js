5a66b09e73d3456f4a9d61a33fd79144c3bfe5c8
Matthew Wein <matthewjwein@gmail.com>
2016-12-16 13:46:08 -0800

    Fix error when handling default suggestion

--- firefox-code-search/background.js
+++ firefox-code-search/background.js
@@ -1,33 +1,46 @@
 const BASE_URL = "https://searchfox.org/mozilla-central";
 const SEARCH_URL = `${BASE_URL}/search`;
 const SOURCE_URL = `${BASE_URL}/source`;
-const EMPTY_RESULT =
+
+// Provide help text to the user.
 browser.omnibox.setDefaultSuggestion({
-  description: "Search the firefox codebase (e.g. `hello world` | `path:omnibox.js onInputChanged`"
+  description: `Search the firefox codebase
+    (e.g. "hello world" | "path:omnibox.js onInputChanged")`
 });
 
-browser.omnibox.onInputEntered.addListener((text, disposition) => {
-  let url = text;
-  if (!text.startsWith(SOURCE_URL)) {
-    url = `${SEARCH_URL}?q=${text}`;
-  }
-  browser.tabs.query({active: true, currentWindow: true}).then(tabs => {
-    browser.tabs.update(tabs[0].id, {url});
-  });
-});
-
-browser.omnibox.onInputChanged.addListener((text, suggest) => {
-  let url = buildURL(text);
+// Update the suggestions whenever the input is changed.
+browser.omnibox.onInputChanged.addListener((text, addSuggestions) => {
   let headers = new Headers({"Accept": "application/json"});
   let init = {method: 'GET', headers};
+  let url = buildSearchURL(text);
   let request = new Request(url, init);
 
   fetch(request)
     .then(createSuggestionsFromResponse)
-    .then(suggest);
+    .then(addSuggestions);
 });
 
-function buildURL(text) {
+// Open the page based on how the user clicks on a suggestion.
+browser.omnibox.onInputEntered.addListener((text, disposition) => {
+  let url = text;
+  if (!text.startsWith(SOURCE_URL)) {
+    // Update the url if the user clicks on the default suggestion.
+    url = `${SEARCH_URL}?q=${text}`;
+  }
+  switch (disposition) {
+    case "currentTab":
+      browser.tabs.update({url});
+      break;
+    case "newForegroundTab":
+      browser.tabs.create({url});
+      break;
+    case "newBackgroundTab":
+      browser.tabs.create({url, active: false});
+      break;
+  }
+});
+
+function buildSearchURL(text) {
   let path = '';
   let queryParts = [];
   let query = '';
@@ -48,38 +61,42 @@ function buildURL(text) {
 function createSuggestionsFromResponse(response) {
   return new Promise(resolve => {
     let suggestions = [];
-    let contentType = response.headers.get("content-type");
-    if(contentType && contentType.indexOf("application/json") !== -1) {
-      response.json().then(json => {
-        let suggestions = [];
-        if (json.normal) {
-          if (json.normal["Textual Occurrences"]) {
-            let occurrences = json.normal["Textual Occurrences"];
-            occurrences.forEach(occurrence => {
-              suggestions.push({
-                content: `${SOURCE_URL}/${occurrence.path}#${occurrence.lines[0].lno}`,
-                description: occurrence.lines[0].line,
-              });
-            });
-            resolve(suggestions);
-          } else if (json.normal["Files"]) {
-            let files = json.normal["Files"];
-            files.forEach(file => {
-              suggestions.push({
-                content: `${SOURCE_URL}/${file.path}`,
-                description: file.path,
-              });
-            });
-            resolve(suggestions);
-          } else {
-            resolve([{content: SOURCE_URL, description: "no results found"}]);
-          }
-        } else {
-          resolve([{content: SOURCE_URL, description: "no results found"}]);
-        }
+    let suggestionsOnEmptyResults = [{
+      content: SOURCE_URL,
+      description: "no results found"
+    }];
+    response.json().then(json => {
+      if (!json.normal) {
+        return resolve(suggestionsOnEmptyResults);
+      }
+
+      let occurrences = json.normal["Textual Occurrences"];
+      let files = json.normal["Files"];
+
+      if (!occurrences && !files) {
+        return resolve(suggestionsOnEmptyResults);
+      }
+
+      if (occurrences) {
+        occurrences.forEach(occurrence => {
+          suggestions.push({
+            content:
+              `${SOURCE_URL}/${occurrence.path}#${occurrence.lines[0].lno}`,
+            description: occurrence.lines[0].line,
+          });
+        });
+        return resolve(suggestions);
+      }
+
+      // There won't be any textual occurrences if the "path:" prefix is used.
+      files.forEach(file => {
+        suggestions.push({
+          content: `${SOURCE_URL}/${file.path}`,
+          description: file.path,
+        });
       });
-    } else {
-      resolve([{content: SOURCE_URL, description: "no results found"}]);
-    }
+      return resolve(suggestions);
+    });
   });
 }
+
