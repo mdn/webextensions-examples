048de5e470aaa316e969fb2f1db946940ff5a08e
Matthew Wein <matthewjwein@gmail.com>
2016-12-16 13:05:42 -0800

    Resolved pull request comments and did some minor refactoring

--- firefox-code-search/background.js
+++ firefox-code-search/background.js
@@ -1,18 +1,30 @@
-const SRC_URL = "http://searchfox.org/mozilla-central";
-const PATH_PREFIX = "path:";
-
-chrome.omnibox.setDefaultSuggestion({
-  description: "Search the firefox codebase (e.g. `hello world` | `path:omnibox.js hello world`"
+const BASE_URL = "https://searchfox.org/mozilla-central";
+const SEARCH_URL = `${BASE_URL}/search`;
+const SOURCE_URL = `${BASE_URL}/source`;
+const EMPTY_RESULT =
+browser.omnibox.setDefaultSuggestion({
+  description: "Search the firefox codebase (e.g. `hello world` | `path:omnibox.js onInputChanged`"
 });
 
-chrome.omnibox.onInputEntered.addListener((text, disposition) => {
+browser.omnibox.onInputEntered.addListener((text, disposition) => {
   let url = text;
-  if (!text.startsWith(SRC_URL)) {
-    url = `${SRC_URL}/search?q=${text}+-path%3Aobj-&redirect=false`
+  if (!text.startsWith(SOURCE_URL)) {
+    url = `${SEARCH_URL}?q=${text}`;
   }
-  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-    chrome.tabs.update(tabs[0].id, {url});
+  browser.tabs.query({active: true, currentWindow: true}).then(tabs => {
+    browser.tabs.update(tabs[0].id, {url});
   });
+});
+
+browser.omnibox.onInputChanged.addListener((text, suggest) => {
+  let url = buildURL(text);
+  let headers = new Headers({"Accept": "application/json"});
+  let init = {method: 'GET', headers};
+  let request = new Request(url, init);
+
+  fetch(request)
+    .then(createSuggestionsFromResponse)
+    .then(suggest);
 });
 
 function buildURL(text) {
@@ -21,63 +33,53 @@ function buildURL(text) {
   let query = '';
   let parts = text.split(' ');
 
-  parts.forEach((part, i) => {
-    if (part.startsWith(PATH_PREFIX)) {
-      path = part.slice(PATH_PREFIX.length);
+  parts.forEach(part => {
+    if (part.startsWith("path:")) {
+      path = part.slice(5);
     } else {
       queryParts.push(part);
     }
   });
 
   query = queryParts.join(' ');
-  return `${SRC_URL}/search?q=${query}&case=false&regexp=${isRegex}&path=${path}`;
+  return `${SEARCH_URL}?q=${query}&path=${path}`;
 }
 
-chrome.omnibox.onInputChanged.addListener((text, suggest) => {
-  let url = buildURL(text);
-  let headers = new Headers({"Accept": "application/json"});
-  let init = {method: 'GET', headers};
-  let request = new Request(url, init);
-
-  fetch(request).then(response => {
+function createSuggestionsFromResponse(response) {
+  return new Promise(resolve => {
+    let suggestions = [];
     let contentType = response.headers.get("content-type");
     if(contentType && contentType.indexOf("application/json") !== -1) {
-      response.json().then(function(json) {
+      response.json().then(json => {
         let suggestions = [];
         if (json.normal) {
           if (json.normal["Textual Occurrences"]) {
-            let occurrences = json.normal["Textual Occurrences"].slice(0, 3);
+            let occurrences = json.normal["Textual Occurrences"];
             occurrences.forEach(occurrence => {
               suggestions.push({
-                content: `http://searchfox.org/mozilla-central/source/${occurrence.path}#${occurrence.lines[0].lno}`,
-                description: `Occurrence: ${occurrence.lines[0].line}`,
+                content: `${SOURCE_URL}/${occurrence.path}#${occurrence.lines[0].lno}`,
+                description: occurrence.lines[0].line,
               });
             });
-          }
-          if (json.normal["Files"]) {
-            let files = json.normal["Files"].slice(0, 7);
+            resolve(suggestions);
+          } else if (json.normal["Files"]) {
+            let files = json.normal["Files"];
             files.forEach(file => {
               suggestions.push({
-                content: `http://searchfox.org/mozilla-central/source/${file.path}`,
-                description: `File: ${file.path}`,
+                content: `${SOURCE_URL}/${file.path}`,
+                description: file.path,
               });
             });
+            resolve(suggestions);
+          } else {
+            resolve([{content: SOURCE_URL, description: "no results found"}]);
           }
-        }
-        if (suggestions.length) {
-          suggest(suggestions);
         } else {
-          suggest([{
-            content: "http://searchfox.org/mozilla-central/source",
-            description: "no results found",
-          }]);
+          resolve([{content: SOURCE_URL, description: "no results found"}]);
         }
       });
     } else {
-      suggest([{
-        content: "http://searchfox.org/mozilla-central/source",
-        description: "no results found",
-      }]);
+      resolve([{content: SOURCE_URL, description: "no results found"}]);
     }
   });
-});
+}
