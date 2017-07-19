b6f98daf5a39db902d2b4fa83e242cb4bd73b03e
hellosct1 <hellosct1@gmail.com>
2017-07-19 19:50:17 +0200

    correction next

--- devtools-panels/background_scripts/background.js
+++ devtools-panels/background_scripts/background.js
@@ -3,8 +3,8 @@
 When we receive the message, execute the given script in the given
 tab.
 */
-function handleMessage(request,sender) {
-
+function handleMessage(request, sender, sendResponse) {
+ 
   if (sender.url != browser.runtime.getURL("/devtools/panel/panel.html")) {
     return;
   }
