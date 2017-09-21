6d22dbe5998476219a925a1bcbd056989228c8e4
Andy <amckay@mozilla.com>
2017-09-21 16:48:10 -0700

    update as per feedback

--- find-across-tabs/find.js
+++ find-across-tabs/find.js
@@ -1,6 +1,7 @@
 let backgroundPage = browser.extension.getBackgroundPage();
 
 document.getElementById("find-form").addEventListener("submit", function(e) {
+  // Send the query from the form to the background page.
   backgroundPage.find(document.getElementById("find-input").value);
   e.preventDefault();
 });
@@ -8,10 +9,12 @@ document.getElementById("find-form").addEventListener("submit", function(e) {
 let results = document.getElementById("result-list");
 
 function handleMessage(request, sender, response) {
+  // Handle responses coming back from the background page.
   if (request.msg === "clear-results") {
     results.innerHTML = "";
   }
   if (request.msg === "found-result") {
+    // List out responses from the background page as they come in.
     let li = document.createElement("li");
     li.innerText = `Tab id: ${request.id} at url: ${request.url} had ${request.count} hits.`;
     results.appendChild(li);
