8b5f320c1ac20bc6be4debd31d707ebe345788c2
Andy McKay <amckay@mozilla.com>
2017-12-08 09:53:33 -0800

    add in navigation types (#305)

--- navigation-stats/background.js
+++ navigation-stats/background.js
@@ -1,17 +1,30 @@
 // Load existent stats with the storage API.
-var gettingStoredStats = browser.storage.local.get("hostNavigationStats");
+var gettingStoredStats = browser.storage.local.get();
+
 gettingStoredStats.then(results => {
   // Initialize the saved stats if not yet initialized.
-  if (!results.hostNavigationStats) {
+  if (!results.stats) {
     results = {
-      hostNavigationStats: {}
+      host: {},
+      type: {}
     };
   }
 
-  const {hostNavigationStats} = results;
-
   // Monitor completed navigation events and update
   // stats accordingly.
+  browser.webNavigation.onCommitted.addListener((evt) => {
+    if (evt.frameId !== 0) {
+      return;
+    }
+
+    let transitionType = evt.transitionType;
+    results.type[transitionType] = results.type[transitionType] || 0;
+    results.type[transitionType]++;
+
+    // Persist the updated stats.
+    browser.storage.local.set(results);
+  });
+
   browser.webNavigation.onCompleted.addListener(evt => {
     // Filter out any sub-frame related navigation event
     if (evt.frameId !== 0) {
@@ -20,8 +33,8 @@ gettingStoredStats.then(results => {
 
     const url = new URL(evt.url);
 
-    hostNavigationStats[url.hostname] = hostNavigationStats[url.hostname] || 0;
-    hostNavigationStats[url.hostname]++;
+    results.host[url.hostname] = results.host[url.hostname] || 0;
+    results.host[url.hostname]++;
 
     // Persist the updated stats.
     browser.storage.local.set(results);
