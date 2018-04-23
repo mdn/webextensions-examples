8694314d5a328616f028bb71e4d6bbd352375440
Daniel Dawson <danielcdawson@gmail.com>
2018-04-23 14:32:28 -0700

    Fix end tag in HTML fragment. (#355)

--- user-script/popup/user-script.js
+++ user-script/popup/user-script.js
@@ -4,7 +4,7 @@ const hostsInput =  document.querySelector("#hosts");
 const codeInput =  document.querySelector("#code");
 
 const defaultHosts = "*://*.org/*";
-const defaultCode = "document.body.innerHTML = '<h1>This page has been eaten<h1>'";
+const defaultCode = "document.body.innerHTML = '<h1>This page has been eaten</h1>'";
 
 hostsInput.value = defaultHosts;
 codeInput.value = defaultCode;
