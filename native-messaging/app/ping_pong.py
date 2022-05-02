07189927d4e9043ce51cf041869ffb75ef3f0b80
guest271314 <guest271314@gmail.com>
2022-05-01 17:50:15 -0700

    Drop Python 2 support. Fixes #489

--- native-messaging/app/ping_pong.py
+++ native-messaging/app/ping_pong.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env -S python3 -u
+# https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Native_messaging
+# https://github.com/mdn/webextensions-examples/pull/157
+# Note that running python with the `-u` flag is required on Windows,
+# in order to ensure that stdin and stdout are opened in binary, rather
+# than text, mode.
 
 import sys
 import json
@@ -13,7 +18,7 @@ try:
             sys.exit(0)
         messageLength = struct.unpack('@I', rawLength)[0]
         message = sys.stdin.buffer.read(messageLength).decode('utf-8')
-        return json.loads(message)
+        return json.loads(message)["message"]
 
     # Encode a message for transmission,
     # given its content.
@@ -31,32 +36,8 @@ try:
     while True:
         receivedMessage = getMessage()
         if receivedMessage == "ping":
-            sendMessage(encodeMessage("pong3"))
-except AttributeError:
-    # Python 2.x version (if sys.stdin.buffer is not defined)
-    # Read a message from stdin and decode it.
-    def getMessage():
-        rawLength = sys.stdin.read(4)
-        if len(rawLength) == 0:
-            sys.exit(0)
-        messageLength = struct.unpack('@I', rawLength)[0]
-        message = sys.stdin.read(messageLength)
-        return json.loads(message)
-
-    # Encode a message for transmission,
-    # given its content.
-    def encodeMessage(messageContent):
-        encodedContent = json.dumps(messageContent)
-        encodedLength = struct.pack('@I', len(encodedContent))
-        return {'length': encodedLength, 'content': encodedContent}
-
-    # Send an encoded message to stdout
-    def sendMessage(encodedMessage):
-        sys.stdout.write(encodedMessage['length'])
-        sys.stdout.write(encodedMessage['content'])
-        sys.stdout.flush()
-
-    while True:
-        receivedMessage = getMessage()
-        if receivedMessage == "ping":
-            sendMessage(encodeMessage("pong2"))
+            sendMessage(encodeMessage({"message":"pong3"}))
+except Exception as e:
+    sys.stdout.buffer.flush()
+    sys.stdin.buffer.flush()
+    sys.exit(0)
