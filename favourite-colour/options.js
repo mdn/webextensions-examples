b2e1f8dd91f9cdf2659877e0a79e7a0d433d8bb6
rebloor <git@sherpa.co.nz>
2024-11-01 14:35:21 +1300

    Suggestion from feedback
    Co-authored-by: Simeon Vincent <svincent@gmail.com>

--- favourite-colour/options.js
+++ favourite-colour/options.js
@@ -6,12 +6,16 @@ async function saveOptions(e) {
 }
 
 async function restoreOptions() {
-	try {
-		let res = await browser.storage.managed.get('colour');
-		document.querySelector("#managed-colour").innerText = res.colour;
-	} catch(error) {
-		console.log(JSON.stringify(error));
-	}
+ try {
+   let res = await browser.storage.managed.get('colour');
+   document.querySelector("#managed-colour").innerText = res.colour || "Could not find 'colour' in managed storage.";
+ } catch(error) {
+   if (error.message === "Managed storage manifest not found") {
+     document.querySelector("#managed-colour").innerText = "Managed storage manifest not found. Make sure it's stored in an appropriate location.";
+   } else {
+     document.querySelector("#managed-colour").innerText = `Unexpected managed storage error: ${error.message}`;
+   }
+ }
 
   res = await browser.storage.sync.get('colour');
   document.querySelector("#colour").value = res.colour || 'Firefox red';
