da2172c3614e910aecfc57a15e347347b2a86e1a
Richard Bloor <git@sherpa.co.nz>
2025-11-12 12:04:18 +1300

    MDN get started tutorial examples to manifest V3

--- beastify/popup/choose_beast.js
+++ beastify/popup/choose_beast.js
@@ -33,7 +33,7 @@ function listenForClicks() {
      * send a "beastify" message to the content script in the active tab.
      */
     function beastify(tabs) {
-      browser.tabs.insertCSS({code: hidePage}).then(() => {
+      browser.scripting.insertCSS({code: hidePage}).then(() => {
         const url = beastNameToURL(e.target.textContent);
         browser.tabs.sendMessage(tabs[0].id, {
           command: "beastify",
@@ -47,7 +47,7 @@ function listenForClicks() {
      * send a "reset" message to the content script in the active tab.
      */
     function reset(tabs) {
-      browser.tabs.removeCSS({code: hidePage}).then(() => {
+      browser.scripting.removeCSS({code: hidePage}).then(() => {
         browser.tabs.sendMessage(tabs[0].id, {
           command: "reset",
         });
