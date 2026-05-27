30954488af3e3de2d65f00bfaf450717429db22b
Richard Bloor <git@sherpa.co.nz>
2026-05-28 07:08:29 +1200

    menu-demo to Manifest v3

--- menu-demo/background.js
+++ menu-demo/background.js
@@ -1,16 +1,4 @@
 /*
-Called when the item has been created, or when creation failed due to an error.
-We'll just log success/failure here.
-*/
-function onCreated() {
-  if (browser.runtime.lastError) {
-    console.log(`Error: ${browser.runtime.lastError}`);
-  } else {
-    console.log("Item created successfully");
-  }
-}
-
-/*
 Called when the item has been removed.
 We'll just log success here.
 */
@@ -29,87 +17,91 @@ function onError(error) {
 /*
 Create all the context menu items.
 */
-browser.menus.create({
-  id: "log-selection",
-  title: browser.i18n.getMessage("menuItemSelectionLogger"),
-  contexts: ["selection"]
-}, onCreated);
+browser.runtime.onInstalled.addListener(() => {
+  browser.menus.create({
+    id: "log-selection",
+    title: browser.i18n.getMessage("menuItemSelectionLogger"),
+    contexts: ["selection"]
+  });
 
-browser.menus.create({
-  id: "remove-me",
-  title: browser.i18n.getMessage("menuItemRemoveMe"),
-  contexts: ["all"]
-}, onCreated);
+  browser.menus.create({
+    id: "remove-me",
+    title: browser.i18n.getMessage("menuItemRemoveMe"),
+    contexts: ["all"]
+  });
 
-browser.menus.create({
-  id: "separator-1",
-  type: "separator",
-  contexts: ["all"]
-}, onCreated);
+  browser.menus.create({
+    id: "separator-1",
+    type: "separator",
+    contexts: ["all"]
+  });
 
-browser.menus.create({
-  id: "greenify",
-  type: "radio",
-  title: browser.i18n.getMessage("menuItemGreenify"),
-  contexts: ["all"],
-  checked: true,
-  icons: {
-    "16": "icons/paint-green-16.png",
-    "32": "icons/paint-green-32.png"
-  }
-}, onCreated);
+  browser.menus.create({
+    id: "greenify",
+    type: "radio",
+    title: browser.i18n.getMessage("menuItemGreenify"),
+    contexts: ["all"],
+    checked: true,
+    icons: {
+      "16": "icons/paint-green-16.png",
+      "32": "icons/paint-green-32.png"
+    }
+  });
 
-browser.menus.create({
-  id: "bluify",
-  type: "radio",
-  title: browser.i18n.getMessage("menuItemBluify"),
-  contexts: ["all"],
-  checked: false,
-  icons: {
-    "16": "icons/paint-blue-16.png",
-    "32": "icons/paint-blue-32.png"
-  }
-}, onCreated);
+  browser.menus.create({
+    id: "bluify",
+    type: "radio",
+    title: browser.i18n.getMessage("menuItemBluify"),
+    contexts: ["all"],
+    checked: false,
+    icons: {
+      "16": "icons/paint-blue-16.png",
+      "32": "icons/paint-blue-32.png"
+    }
+  });
 
-browser.menus.create({
-  id: "separator-2",
-  type: "separator",
-  contexts: ["all"]
-}, onCreated);
+  browser.menus.create({
+    id: "separator-2",
+    type: "separator",
+    contexts: ["all"]
+  });
 
-browser.menus.create({
-  id: "check-uncheck",
-  type: "checkbox",
-  title: browser.i18n.getMessage("menuItemUncheckMe"),
-  contexts: ["all"],
-  checked: true,
-}, onCreated);
+  browser.menus.create({
+    id: "check-uncheck",
+    type: "checkbox",
+    title: browser.i18n.getMessage("menuItemUncheckMe"),
+    contexts: ["all"],
+    checked: true,
+  });
 
-browser.menus.create({
-  id: "open-sidebar",
-  title: browser.i18n.getMessage("menuItemOpenSidebar"),
-  contexts: ["all"],
-  command: "_execute_sidebar_action"
-}, onCreated);
+  browser.menus.create({
+    id: "open-sidebar",
+    title: browser.i18n.getMessage("menuItemOpenSidebar"),
+    contexts: ["all"],
+    command: "_execute_sidebar_action"
+  });
 
-browser.menus.create({
-  id: "tools-menu",
-  title: browser.i18n.getMessage("menuItemToolsMenu"),
-  contexts: ["tools_menu"],
-}, onCreated);
+  browser.menus.create({
+    id: "tools-menu",
+    title: browser.i18n.getMessage("menuItemToolsMenu"),
+    contexts: ["tools_menu"],
+  });
+});
 
 /*
 Set a colored border on the document in the given tab.
 
-Note that this only work on normal web pages, not special pages
+Note that this only works on normal web pages, not special pages
 like about:debugging.
 */
-let blue = 'document.body.style.border = "5px solid blue"';
-let green = 'document.body.style.border = "5px solid green"';
+const blue = "5px solid blue";
+const green = "5px solid green";
 
 function borderify(tabId, color) {
-  browser.tabs.executeScript(tabId, {
-    code: color
+  browser.scripting.executeScript({
+    target: { tabId },
+    func: (border) => { document.body.style.border = border; },
+    args: [color]
   });
 }
 
@@ -138,8 +130,7 @@ browser.menus.onClicked.addListener((info, tab) => {
       console.log(info.selectionText);
       break;
     case "remove-me":
-      let removing = browser.menus.remove(info.menuItemId);
-      removing.then(onRemoved, onError);
+      browser.menus.remove(info.menuItemId).then(onRemoved, onError);
       break;
     case "bluify":
       borderify(tab.id, blue);
